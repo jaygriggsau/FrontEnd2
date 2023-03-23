88c1ccfe0532ceb6b9cc29afdcfc5f324c20123c
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-03-23 23:56:37 +0000

    front end chat bot fixes

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,59 +1,36 @@
 import React, { useState } from 'react';
 
-//Grid
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-//Forms
-import { Form, Button, FormControl, Spinner } from 'react-bootstrap';
+function ChatBot() {
+  const [message, setMessage] = useState('');
 
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-function ChatBot(props) {
-        const [message, setMessage] = useState('');
-        const [response, setResponse] = useState('');
-        const [spinner, setSpinner] = useState(false)
+    const res = await fetch('https://adblitzserver.herokuapp.com/adstyles/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    });
 
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-            setSpinner(true)
+    const data = await res.json();
 
-            try {
-              const res = await fetch(`https://adblitzserver.herokuapp.com/adstyles/chat`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ message }),
-              });
-              const json = await res.json();
-              var responseText = json.data.split(/\n\n|\n/);
-              props.setResponse(responseText); // Update the response state in the parent App component
-              
-              props.onGenerateAd(); // Call the onGenerateAd prop to update the showOutput state in the parent App component
-            } catch (err) {
-              console.error(err);
-            }
-          }; 
-        return (
-          <Container>
-            <h3 className='text-center'>Chat Bot</h3>
-            <Row>
-              <Col sm={12} lg={5}>
-                <Form onSubmit={handleSubmit}>
-            <Form.Label>Start Talking</Form.Label>
-            <FormControl
-                type="text"
-                value={message}
-                placeholder="Say Hello"
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <Button variant="primary" className='submitbtn text-center' type="submit">Generate Ad!</Button>
-            </Form>
-            {spinner && <Spinner animation="grow" />}
-        </Col>
-      </Row>
-    </Container>
+    // create a new p tag with the response data
+    const newPTag = document.createElement('p');
+    newPTag.textContent = data.message;
+    document.body.appendChild(newPTag);
+
+    // reset the text input form
+    setMessage('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={message} onChange={(event) => setMessage(event.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default ChatBot
+export default ChatBot;
